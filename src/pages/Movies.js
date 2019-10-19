8a247941d32b6201c4eb4a1161cfc069a92c7f3b
sakshambakshi <sakshambakshi@users.noreply.github.com>
2019-10-19 21:35:44 +0530

    Added SubComponet in Movies

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Breadcrumb from '../components/Breadcrumb.js'
+import SearchBar from '../components/SearchBar.js'
+import MoviesList from '../components/MoviesList.js'
+import Container from '@material-ui/core/Container';
+import Grid from "@material-ui/core/Grid";
 
 export default function Movies() {
     return (
-        <div>
-            dsaf
-        </div>
+        <Container maxWidth="lg">
+        <Grid container spacing={12}>
+        
+            <Breadcrumb />
+            <SearchBar />
+            <MoviesList />
+        
+        </Grid>
+        </Container>
     )
 }
