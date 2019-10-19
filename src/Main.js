6ebbee4a4193f50c48a1fc13cb98f41bf74d2cb0
sakshambakshi <sakshambakshi@users.noreply.github.com>
2019-10-19 19:52:01 +0530

    Inport stylesheet at main.js

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import Home from './pages/Home'
+import Home from './pages/Home';
+import './assets/css/style.css';
 export default class Main extends Component {
   render() {
     return (
