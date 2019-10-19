070cf3028c971231daf08ef07fbbf05200c90edb
sakshambakshi <sakshambakshi@users.noreply.github.com>
2019-10-19 17:51:14 +0530

    Working on banner and adding react-swipeable-views

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Navbar from '../components/Navbar';
+import Banner from '../components/Banner';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <Banner />
       </div>
     )
   }
