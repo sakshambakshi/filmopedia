839e9438ac5f9871adfd1ab0278cb5570f1e1fa4
sakshambakshi <sakshambakshi@users.noreply.github.com>
2019-10-19 17:06:54 +0530

    Created Navbar.js for upper navigations(Topheader [location:components/Navbar.js])

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-export default class Home extends Component {
+export default class Navbar extends Component {
   render() {
     return (
       <div>
-        <h1></h1>
+        
       </div>
     )
   }
