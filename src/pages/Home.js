4e38a617edc5544bdf2cc05eb984741f2d7440f3
sakshambakshi <sakshambakshi@users.noreply.github.com>
2019-10-19 17:46:37 +0530

    Added top navbar till now

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Navbar from '../components/Navbar';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <h1></h1>
+        <Navbar />
       </div>
     )
   }
