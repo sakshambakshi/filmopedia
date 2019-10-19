566ff73ccaa9e58ff9a92dee41b77de4df4eb257
sakshambakshi <sakshambakshi@users.noreply.github.com>
2019-10-19 20:48:44 +0530

    Added routes for home and movies and make the navbar common for all pages

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,32 @@
-import React, { Component } from 'react'
-import Home from './pages/Home';
-import './assets/css/style.css';
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import "./assets/css/style.css";
 export default class Main extends Component {
   render() {
     return (
-      <div>
-        <Home />
-      </div>
-    )
+      <React.Fragment>
+        <Navbar />
+        <Router>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/movies">
+              <Movies />
+            </Route>
+          </Switch>
+        </Router>
+      </React.Fragment>
+    );
   }
 }
