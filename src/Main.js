4ce71d8b40fece1ddbc04011cfb2e4110da67aa2
sakshambakshi <sakshambakshi@users.noreply.github.com>
2019-10-19 17:03:54 +0530

    Created Main.js for entry point in our code no changes will be done in app.js

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Home from './pages/Home'
+export default class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Home />
+      </div>
+    )
+  }
+}
