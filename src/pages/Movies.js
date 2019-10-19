4432cffca6be45ecea72361644456781042535ce
sakshambakshi <sakshambakshi@users.noreply.github.com>
2019-10-19 21:55:45 +0530

    Created Movie List

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -4,15 +4,53 @@ import SearchBar from '../components/SearchBar.js'
 import MoviesList from '../components/MoviesList.js'
 import Container from '@material-ui/core/Container';
 import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles(theme => ({
+    card: {
+      maxWidth: 345
+    },
+    media: {
+      height: 0,
+      paddingTop: "56.25%" // 16:9
+    },
+    mtcustom:{
+        marginTop: 15
+    },
+    expand: {
+      transform: "rotate(0deg)",
+      marginLeft: "auto",
+      transition: theme.transitions.create("transform", {
+        duration: theme.transitions.duration.shortest
+      })
+    },
+    expandOpen: {
+      transform: "rotate(180deg)"
+    },
+
+  }));
+  
 export default function Movies() {
-    return (
+    const classes = useStyles();
+    return (        
         <Container maxWidth="lg">
-        <Grid container spacing={12}>
+        <Grid container spacing={10}>
         
             <Breadcrumb />
             <SearchBar />
+            <Grid className={classes.mtcustom} container spacing={3}>
             <MoviesList />
+            <MoviesList />
+            <MoviesList />
+            <MoviesList />
+            <MoviesList />
+            <MoviesList />
+            <MoviesList />
+            <MoviesList />
+            <MoviesList />
+            <MoviesList />
+            <MoviesList />
+            </Grid>
         
         </Grid>
         </Container>
