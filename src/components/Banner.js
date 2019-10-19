c0e163efadfe33c6e949aaf94a804c4f416f6750
sakshambakshi <sakshambakshi@users.noreply.github.com>
2019-10-19 18:05:23 +0530

    Created Carousel Slider as Bootstrap

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -15,33 +15,23 @@ const tutorialSteps = [
   {
     label: 'San Francisco – Oakland Bay Bridge, United States',
     imgPath:
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+      'http://crystalstab.com/assets/images/banner/banner5.jpg',
   },
   {
     label: 'Bird',
     imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+      'http://crystalstab.com/assets/images/banner/banner1.jpg',
   },
   {
     label: 'Bali, Indonesia',
     imgPath:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
-  },
-  {
-    label: 'NeONBRAND Digital Marketing, Las Vegas, United States',
-    imgPath:
-      'https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60',
-  },
-  {
-    label: 'Goč, Serbia',
-    imgPath:
-      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-  },
+      'http://crystalstab.com/assets/images/banner/banner4.jpg',
+  }
 ];
 
 const useStyles = makeStyles(theme => ({
   root: {
-    maxWidth: 400,
+    maxWidth: "100%",
     flexGrow: 1,
   },
   header: {
@@ -52,9 +42,9 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: theme.palette.background.default,
   },
   img: {
-    height: 255,
+    height: '350',
     display: 'block',
-    maxWidth: 400,
+    maxWidth: '100%',
     overflow: 'hidden',
     width: '100%',
   },
@@ -80,9 +70,7 @@ function Banner() {
 
   return (
     <div className={classes.root}>
-      <Paper square elevation={0} className={classes.header}>
-        <Typography>{tutorialSteps[activeStep].label}</Typography>
-      </Paper>
+     
       <AutoPlaySwipeableViews
         axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
         index={activeStep}
